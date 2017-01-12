ef5df69eefc530dceb8d3f1574d6088e4c51419f
zhuqj1 <zhuqj1@motorola.com>
2017-01-12 16:30:16 +0800

    IKSECURITY-206 : Port Root Detect to JB
    Change-Id: I3585fa970b76e161938285df45c86bc3fcedcb3b
    Reviewed-on: https://gerrit.mot.com/940266
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: quanjun zhu <zhuqj1@motorola.com>

--- minzip/Android.mk
+++ minzip/Android.mk
@@ -22,3 +22,22 @@ LOCAL_CLANG := true
 LOCAL_CFLAGS += -Werror -Wall
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    Hash.c \
+	SysUtil.c \
+	DirUtil.c \
+	Inlines.c \
+	Zip.c
+
+LOCAL_C_INCLUDES += \
+	external/zlib \
+	external/safe-iop/include
+
+LOCAL_MODULE := libminzip
+
+LOCAL_CFLAGS += -Wall
+
+include $(BUILD_HOST_STATIC_LIBRARY)
